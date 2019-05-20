8ac8da548ec04646dc33799064dd97ce783ba9c3
newrishman <safarovrishat@gmail.com>
2019-05-20 19:06:43 +0300

    изменил цикл

--- src/Roulette/Requlations1.java
+++ src/Roulette/Requlations1.java
@@ -4,26 +4,27 @@ import java.io.*;
 
 public class Requlations1 {
     protected static void forOne() throws IOException {
-        int[] bullet;
-        int count = 0;
+
+
 
         AllPrint.all(4);
         AllPrint.all(5);
         UserBang.start();
 
-        bullet = Bullet.bullet();
-
-        for (int i = 0; i < 6; i++) {
+        int[] bullet = Bullet.bullet();
+        int count = 0;
+        for (int i:bullet){
             AllPrint.all(6);
             UserBang.bang();
             count++;
-            if (bullet[i] == 1) {
+            if (i == 1) {
                 AllPrint.all(10);
                 break;
             } else {
                 AllPrint.all(9);
             }
         }
+
         AllPrint.all(11);
         System.out.print(count);
     }
