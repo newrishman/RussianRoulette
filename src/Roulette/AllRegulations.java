fd2471ab6daaffadbf4b54b6a19b5ec5bc646a56
newrishman <safarovrishat@gmail.com>
2019-05-20 21:36:32 +0300

    Поправил правила, теперь только два человека могут играть

--- src/Roulette/AllRegulations.java
+++ src/Roulette/AllRegulations.java
@@ -9,6 +9,7 @@ class AllRegulations {
         if (choice > 0 && choice <= 2) {
             return choice;
         } else {
+            AllPrint.all(12);
             return 0;
         }
     }
@@ -16,9 +17,10 @@ class AllRegulations {
     protected static int quantity() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int quantity = Integer.parseInt(reader.readLine());
-        if (quantity > 0 && quantity <= 3) {
+        if (quantity > 0 && quantity <= 2) {
             return quantity;
         } else {
+            AllPrint.all(12);
             return 0;
         }
     }
