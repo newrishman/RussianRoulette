85aa2b05e72d9bbbaa0831ca2b5dc7031694f7e3
newrishman <safarovrishat@gmail.com>
2019-05-26 00:02:32 +0300

    Maven

---
+++ src/main/java/Start.java
@@ -0,0 +1,11 @@
+
+
+import Roulette.SuperSeriouslyClass;
+
+import java.io.*;
+
+public class Start {
+    public static void main(String[] args) throws IOException {
+        SuperSeriouslyClass.start();
+    }
+}
