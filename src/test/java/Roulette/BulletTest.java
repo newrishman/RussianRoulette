bc66369dba553ccadfe22d63ac013a5d46601a77
newrishman <safarovrishat@gmail.com>
2019-06-01 00:03:02 +0300

    Покрыл тестом класс Bullet

--- src/test/java/Roulette/BulletTest.java
+++ src/test/java/Roulette/BulletTest.java
@@ -1,7 +1,31 @@
 package Roulette;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BulletTest {
+    @Test
+    public void testBullet1() {
+        Bullet bullet = new Bullet();
+        int[] test = bullet.bullet();
+        int x = 0;
+        for (int i = 0; i < test.length; i++) {
+            x += test[i];
+        }
+        assertEquals("Сумма массива должна быть единицей", 1, x);
+    }
+@Test
+    public void testBullet2() {
+        Bullet bullet = new Bullet();
+        int[] test = bullet.bullet();
+        boolean x = false;
+        for (int i = 0; i < test.length; i++) {
+            if (test[i] == 0 || test[i] == 1) {
+                x = true;
+            }
+        }
+        assertEquals("В массиве должны быть только нули и единицы", true, x);
+    }
 
 }
