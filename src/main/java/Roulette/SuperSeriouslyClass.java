4eb1ac71204e4996eb4be658d8f191061be5b980
newrishman <safarovrishat@gmail.com>
2019-06-02 01:21:28 +0300

    Изменил класс SuperSeriously, чтобы он принимал параметры

--- src/main/java/Roulette/SuperSeriouslyClass.java
+++ src/main/java/Roulette/SuperSeriouslyClass.java
@@ -4,16 +4,33 @@ import java.io.*;
 
 public class SuperSeriouslyClass {
 
-    public void start() throws IOException {
-        AllPrint allPrint = new AllPrint(System.out, new AllText());
+    public int getA() throws IOException {
+
         AllRegulations allRegulations = new AllRegulations();
-        Classic classic = new Classic();
-        Again again = new Again();
+        AllPrint allPrint = new AllPrint(System.out, new AllText());
+
         allPrint.all(1);
         allPrint.all(2);
+
         int a = allRegulations.choice(allRegulations.getChoice());
+        return a;
+    }
+
+    public int getB() throws IOException {
+
+        AllRegulations allRegulations = new AllRegulations();
+        AllPrint allPrint = new AllPrint(System.out, new AllText());
+
         allPrint.all(3);
-        int b = allRegulations.quantity(allRegulations.getQuantity());
+
+        int b = allRegulations.choice(allRegulations.getQuantity());
+        return b;
+    }
+
+    public void start(int a, int b) throws IOException {
+
+        Classic classic = new Classic();
+        Again again = new Again();
 
         if (a == 1 && b == 1) {
             classic.singlePlayer();
--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -6,7 +6,12 @@ import java.io.*;
 
 public class Start {
     public static void main(String[] args) throws IOException {
+
         SuperSeriouslyClass superSeriouslyClass = new SuperSeriouslyClass();
-        superSeriouslyClass.start();
+
+        int a = superSeriouslyClass.getA();
+        int b = superSeriouslyClass.getB();
+
+        superSeriouslyClass.start(a, b);
     }
 }
