d2cc1e13866fe14e693eb76556ab100f41c5a336
newrishman <safarovrishat@gmail.com>
2019-06-02 01:22:28 +0300

    Не создал тест void класса

--- src/test/java/Roulette/SuperSeriouslyClassTest.java
+++ src/test/java/Roulette/SuperSeriouslyClassTest.java
@@ -3,5 +3,6 @@ package Roulette;
 import static org.junit.Assert.*;
 
 public class SuperSeriouslyClassTest {
+    // понятия не имею, как писать тест для void метода, вызываюзего другой метод (((
 
 }
