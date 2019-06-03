824768dcdda2afce14838d92627f19755af20990
newrishman <safarovrishat@gmail.com>
2019-06-03 11:29:15 +0300

    Не создал тест void класса

--- src/test/java/Roulette/AllRegulationsTest.java
+++ src/test/java/Roulette/AllRegulationsTest.java
@@ -9,18 +9,18 @@ public class AllRegulationsTest {
     public void testAllRegulationsChoice() {
         AllRegulations allRegulations = new AllRegulations();
         int x = allRegulations.choice(2);
-        assertEquals("Если входные параметры 1 или 2, возвращается входной параметр", 2, 2);
+        assertEquals("Если входные параметры 1 или 2, возвращается входной параметр", 2, x);
         x = allRegulations.choice(-1000);
-        assertEquals("Если некорректный параметр, вызывается ошибка и возвращается ноль", 0, 0);
+        assertEquals("Если некорректный параметр, вызывается ошибка и возвращается ноль", 0, x);
     }
 
     @Test
     public void testAllRegulationsQuantity() {
         AllRegulations allRegulations = new AllRegulations();
         int x = allRegulations.quantity(1);
-        assertEquals("Если входные параметры 1 или 2, возвращается входной параметр", 1, 1);
-        x = allRegulations.choice(1000);
-        assertEquals("Если некорректный параметр, вызывается ошибка и возвращается ноль", 0, 0);
+        assertEquals("Если входные параметры 1 или 2, возвращается входной параметр", 1, x);
+        x = allRegulations.quantity(1000);
+        assertEquals("Если некорректный параметр, вызывается ошибка и возвращается ноль", 0, x);
     }
 
 }
