57ba380419326119d805451f2ac7a652fdea6abb
newrishman <safarovrishat@gmail.com>
2019-06-08 01:16:58 +0300

    Зато запускается   ¯\_ (ツ) _/¯

---
+++ src/test/java/Roulette/SuiteTestClass.java
@@ -0,0 +1,13 @@
+package Roulette;
+
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({AllPrintTest.class, AllRegulationsTest.class, BulletTest.class, SuperSeriouslyClassTest.class, UserBangTest.class })
+public class SuiteTestClass {
+
+}
